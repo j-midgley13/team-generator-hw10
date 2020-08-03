e86be0c6f097ef74f0a6eb6b977a722fbd08db32
Jameson Midgley <jameson.midgley@gmail.com>
2020-08-03 16:18:56 -0600

    got tests to run successfully.

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,4 +1,19 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-const Employee = require("/Employee.js")
+const Employee = require("./Employee.js")
+
+class Engineer extends Employee {
+    constructor (name, id, email, github){
+        super (name, id);
+        this.github = github;
+    };
+
+    getRole(){
+        return "Engineer";
+    };
+
+    getGithub(){
+        return this.github;
+    }
+}
 
 module.exports = Engineer;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,4 +1,19 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
-const Employee = require("/Employee.js");
+const Employee = require("./Employee.js");
 
-module.exports = Engineer;
+class Manager extends Employee {
+    constructor (name, id, email, officeNumber){
+        super (name, id);
+        this.officeNumber = officeNumber;
+    };
+
+    getRole(){
+        return "Manager";
+    };
+
+    getOfficeNumber(){
+        return this.officeNumber;
+    }
+};
+
+module.exports = Manager;
